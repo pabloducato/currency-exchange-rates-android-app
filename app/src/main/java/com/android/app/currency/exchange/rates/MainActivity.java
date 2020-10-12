62e79ff128e4565d6f30af00b94b68dbf1b9ebf6
Paweł Kocan <pkocan@onet.pl>
2020-10-12 23:30:55 +0200

    Initial commit

---
+++ app/src/main/java/com/android/app/currency/exchange/rates/MainActivity.java
@@ -0,0 +1,13 @@
+package com.android.app.currency.exchange.rates;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
